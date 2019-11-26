314f21c1be9ac984691e9bab425f8825f5be50a6
16112000hp <16112000hp@gmail.com>
2019-11-26 19:35:07 +0700

    The first code

---
+++ TeduShop.Model/Abstract/Auditable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public abstract class AudiTable : IAudiTable
+    {
+        public DateTime? CreatedDateTable { get; set; }
+
+        [MaxLength(256)]
+        public string CreatedByTable { get; set; }
+        public DateTime? UpdatedDateTable { get; set; }
+
+        [MaxLength(256)]
+        public  string UpdatedByTable { get; set; }
+
+        [MaxLength(256)]
+        public string MetaKeyword { get; set; }
+
+        [MaxLength(256)]
+        public string MetaDescription { get; set; }
+
+        public bool Status { get; set; }
+    }
+}
---
+++ TeduShop.Model/Abstract/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Model.Abstract
+{
+    public interface IAudiTable
+    {
+        DateTime? CreatedDateTable { get; set; }
+        string CreatedByTable { get; set; }
+        DateTime? UpdatedDateTable { get; set; }
+        string UpdatedByTable { get; set; }
+        string MetaKeyword { get; set; }
+        string MetaDescription { get; set; }
+        bool Status { get; set; }
+    }
+}
