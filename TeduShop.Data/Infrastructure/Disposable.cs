f34269728b3b6122dc5c8a2f9b7695731076e6ea
16112000hp <16112000hp@gmail.com>
2019-11-27 09:29:37 +0700

    create Database code first in TeduShop.Data

---
+++ TeduShop.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduShop.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool IsDisposed;
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if(!IsDisposed && disposing)
+            {
+                DisposeCore();
+            }
+            IsDisposed = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+            
+        }
+    }
+}
